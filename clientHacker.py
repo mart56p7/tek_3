7384cae5a1691cd803dd98e2a562cc375e6d17a4
Martin Kjeldgaard Nikolajsen <mart56p7@stud.kea.dk>
2019-10-14 17:16:35 +0200

    m

--- clientHacker.py
+++ clientHacker.py
@@ -62,44 +62,89 @@ class Client:
         self.msgout("talking to server", "init")
         # Lets talk to our server!
         try:
-            # Making the handshake
-            message = 'com-0 ' + self.address
-            self.send(message)
-            rdata = self.receive()
-            self.msgout("com-0 accept " + self.address + " " + rdata, "init")
-            if self.constatus == "connecting":
-                if rdata == ("com-0 accept " + self.address):
-                    self.msgout("Connected", "init")
-                    self.constatus = "connected"
-                    self.send("com-0 accept")
+            self.msgout("")
+            self.msgout("--------------------------------------------------")
+            self.msgout("")
 
-                for i in range(1, self.messages):
-                    if self.constatus == "connected":
-                        if self.msgnum == 0:
-                            self.send("msg-0=hello, i am a new user")
-                        elif rdata == "res-"+str(self.msgnum)+"=I am server":
-                            self.msgnum = self.msgnum + 1
-                            self.send("msg-"+str(self.msgnum)+"=Ok, good to know")
-                        self.msgnum = self.msgnum + 1
-                        rdata = self.receive()
-                        while rdata is None:
-                            # Resend
-                            self.msgout("Resending data", "init")
-                            if self.msgnum == 0:
-                                self.send("msg-0=hello, i am a new user")
-                            else:
-                                self.send("msg-" + str(self.msgnum-1) + "=Ok, good to know")
-                            self.msgout("Waiting for data", "init")
-                            if self.timeout > 5:
-                                raise Exception("Timeout")
-                            rdata = self.receive()
-                # Sending unknown command to server, that forces a close
-            if not self.settings["KeepALive"]:
-                rdata = None
-                while rdata is None:
-                    self.send("close")
-                    rdata = self.receive()
+            try:
+                # Dont send first hand shake
+                self.msgout("START", "Dont send first hand shake")
+                message = 'com-0 accept'
+                self.send(message)
+                rdata = self.receiveWait()
+                # force connection close
+                self.send("close")
+                self.msgout("END", "Dont send first hand shake")
+            except Exception as tmsg:
+                self.msgout(str(tmsg), "Dont send first hand shake")
+                self.msgout("END", "Dont send first hand shake")
 
+            self.msgout("")
+            self.msgout("--------------------------------------------------")
+            self.msgout("")
+
+            try:
+                # Use fake IP Address
+                self.msgout("START", "Use fake IP Address")
+                message = 'com-0 99.0.0.1'
+                self.send(message)
+                rdata = self.receiveWait()
+            except Exception as tmsg:
+                self.msgout(str(tmsg), "Use fake IP Address")
+                self.msgout("END", "Use fake IP Address")
+
+            self.msgout("")
+            self.msgout("--------------------------------------------------")
+            self.msgout("")
+
+            try:
+                # Forget to send last accept
+                self.msgout("START", "Forget to send last accept")
+                message = 'com-0 ' + self.address
+                self.send(message)
+                rdata = self.receiveWait()
+                if self.msgnum == 0:
+                    self.send("msg-0=hello, i am a new user")
+                elif rdata == "res-" + str(self.msgnum) + "=I am server":
+                    self.msgnum = self.msgnum + 1
+                    self.send("msg-" + str(self.msgnum) + "=Ok, good to know")
+                self.msgnum = self.msgnum + 1
+                rdata = self.receiveWait()
+                # force connection close
+                self.send("close")
+                self.msgout("END", "Forget to send last accept")
+            except Exception as tmsg:
+                self.msgout(str(tmsg), "Forget to send last accept")
+                self.msgout("END", "Forget to send last accept")
+
+            self.msgout("")
+            self.msgout("--------------------------------------------------")
+            self.msgout("")
+
+            try:
+                # Send message number out of order
+                self.constatus = "connecting"
+                self.msgout("START", "Send message number out of order")
+                message = 'com-0 ' + self.address
+                self.send(message)
+                rdata = self.receiveWait()
+                if self.constatus == "connecting":
+                    if rdata == ("com-0 accept " + self.address):
+                        self.msgout("Connected", "init")
+                        self.constatus = "connected"
+                        self.send("com-0 accept")
+                        self.send("msg-1000=Ok, good to know")
+                        rdata = self.receiveWait()
+                # force connection close
+                self.send("close")
+                self.msgout("End", "Send message number out of order")
+            except Exception as tmsg:
+                self.msgout(str(tmsg), "Send message number out of order")
+                self.msgout("END", "Send message number out of order")
+
+            self.msgout("")
+            self.msgout("--------------------------------------------------")
+            self.msgout("")
 
         except Exception as errmsg:
             self.msgout("Exception: " + str(errmsg), "init")
@@ -148,28 +193,23 @@ class Client:
             raise Exception("\t\treceive: Connection forcefully closed by server")
         return rdata
 
+    def receiveWait(self):
+        rdata = self.receive()
+        i = 0
+        while rdata is None:
+            rdata = self.receive()
+            if i > 5:
+                raise Exception("\t\treceiveWait: Timeout")
+            else:
+                i = i + 1
+        return rdata
 
 
 def clt(port, messages):
     a = Client(port, messages)
 
 t1 = Thread(target=clt, args=[45000, 20])
-t2 = Thread(target=clt, args=[45001, 50])
-t3 = Thread(target=clt, args=[45002, 100])
-t4 = Thread(target=clt, args=[45003, 1000])
-t5 = Thread(target=clt, args=[45004, 5])
-t6 = Thread(target=clt, args=[45005, 1])
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
 
+t1.start()
 
 t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
-t6.join()
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import socket
 import time
 from threading import Thread
+from datetime import datetime
+import os
 import json
 
 #START#############################
 #START#### SETTINGS ###############
 #START#############################
-debug=False
+debug=True
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 clients = {}
 hostname = socket.gethostname()
@@ -22,6 +23,9 @@ settings = {"MaxPackages" : 25}
 # Loads settings from configfile into our settings dictionary, overriding default settings
 configfile = 'server.ini'
 
+# Log file
+logfile = 'serverlog.log'
+
 #END###############################
 #END###### SETTINGS ###############
 #END###############################
@@ -29,6 +33,19 @@ configfile = 'server.ini'
 #START#############################
 #START#### FUNCTIONS ##############
 #START#############################
+# Log function, defined as the first thing, so its available to all
+def log(msg):
+    global logfile, debug
+    # Check if file exist and either write or append
+    if os.path.exists(logfile):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+    with open(logfile, append_write) as file:
+        now = str(datetime.now())
+        file.write(now + ": " + msg + "\n")
+        if debug:
+            print(now + ": " + msg)
 
 def send(msg, recaddress):
     global sock, clients, debug
@@ -36,7 +53,7 @@ def send(msg, recaddress):
     try:
         result = sock.sendto(msg.encode('ascii'), sendaddress)
     except:
-        if debug: print("\tsend: Closing connection to " + sendaddress[0] + ":" + str(sendaddress[1]))
+        log("\tsend: Closing connection to " + sendaddress[0] + ":" + str(sendaddress[1]))
         clients.pop(sendaddress[0] + "_" + str(sendaddress[1]))
     return result
 
@@ -52,14 +69,14 @@ def acceptPackage(address):
         for t1, t2 in list(clients[address]["packages"].items()):
             if time.time() - t2 > 1:
                 clients[address]["packages"].pop(t1)
-        if debug: print("\tacceptPackage: Packages", packlen)
+        log("\tacceptPackage: Packages" + str(packlen))
         if packlen <= settings["MaxPackages"]:
-            if debug: print("\tacceptPackage: Accepting package", time.time())
+            log("\tacceptPackage: Accepting package " + str(time.time()))
             clients[address]["packages"][str(packlen) + '_' + str(time.time())] = time.time()
             clients[address]["time"] = time.time()
             return True
         else:
-            if debug: print("\tacceptPackage: Rejecting packages", time.time())
+            log("\tacceptPackage: Rejecting packages " + str(time.time()))
             clients[address]["time"] = time.time()
             return False
     else:
@@ -70,29 +87,31 @@ def acceptPackage(address):
 def connectionTimeout():
     global clients, sock
     while True:
-        if debug: print(json.dumps(clients))
+        # log(json.dumps(clients))
         if len(clients) > 0:
             for address, client in list(clients.items()):
                 if time.time() - client['time'] >= 4:
                     client['locked'] = 1
                     # Reset connection
 
-                    if debug: print("\tconnectionTimeout: Reset connection")
+                    log("\tconnectionTimeout: Reset connection")
                     # Used incase of localhost connection
                     saddress = (address, client['port'])
                     send("con-res 0xFE", saddress)
                     closeConnection(address, " Due to timeout")
                     client['locked'] = 0
                 else:
-                    if debug: print("\tconnectionTimeout: Do nothing")
-        # if debug: print("\tconnectionTimeout: checking client")
+                    pass
+                    # log("\tconnectionTimeout: Do nothing")
         time.sleep(0.1)
 
 def closeConnection(address, msg):
     global clients
     if address in clients:
-        print("\tcloseConnection: closed connection to " + address + " after " + str(round(clients[address]["msgnum"]/2)) + " messages." + msg)
+        log("\tcloseConnection: closed connection to " + address + " after " + str(round(clients[address]["msgnum"]/2)) + " messages." + msg)
         clients.pop(address)
+    else:
+        log("\tcloseConnection: closed connection to " + address + ", which wasn't open in the first place." + msg)
 
 #END###############################
 #END###### FUNCTIONS ##############
@@ -101,6 +120,8 @@ def closeConnection(address, msg):
 #START#############################
 #START#### CODE START #############
 #START#############################
+
+log('starting up on '+str(server_address[0])+' port '+str(server_address[1]))
 
 #Loading config file
 with open(configfile) as fp:
@@ -111,10 +132,10 @@ with open(configfile) as fp:
             settings[cmdval[0].strip()] = (int)(cmdval[1].strip());
         line = fp.readline()
 
-# Prints the current client settings
-if debug: print("Current client settings loaded from ", configfile)
+# logs the current client settings
+log("Current client settings loaded from " + configfile)
 for cmd, val in list(settings.items()):
-    if debug: (cmd, ":", val)
+    log(str(cmd) + ":" + str(val))
 
 # timeout manager
 timeoutmanager = Thread(target=connectionTimeout, args=[])
@@ -123,31 +144,35 @@ timeoutmanager.start()
 while True:
     try:
         data, sendaddress = sock.recvfrom(4096)
+
         strdata = data.decode("utf-8")
-        if debug: print("Received " + strdata)
+        log("Received " + strdata)
         # Used incase of localhost connection
         if sendaddress[0] == "127.0.0.1":
             address = IPAddr + ":" + str(sendaddress[1])
         else:
             address = sendaddress[0] + ":" + str(sendaddress[1])
         answer = ""
-        if debug: print(clients)
+        #log(clients)
         if strdata.startswith("com-0 "):
             if(strdata == "com-0 accept") and address in clients: # only continueing if a connecting is in progress to ip adress given in first handshake
-                print(address + " is now connected")
+                log(address + " is now connected")
                 clients[address] = {"time" : time.time(), "state" : "connected", "msgnum" : 0, "port" : sendaddress[1], "locked" : 0, "packages" : {} }
 
             else: #A client can at restart the state of a connection
                 cip = strdata[len("com-0 "):len(data)]
-                if debug: print(cip, address[0])
+                log("Checking client IP claim, IP from package: " + address + " IP claim " + cip)
                 try:
                     socket.inet_aton(cip)
                     clients[cip + ":" + str(sendaddress[1])] =  {"time" : time.time(), "state" : "connecting", "msgnum" : 0, "port" : sendaddress[1], "locked" : 0, "packages" : {} }
-                    if debug: print('\tReceived welcome msg from', cip)
+                    log('Received welcome msg from ' + cip)
                     answer = "com-0 accept " + IPAddr
                 except socket.error:
-                    if debug: print("Invalid ip address given from ", address, cip)
-        elif address in clients and (time.time() - clients[address]["time"]) < 4 and clients[address]['locked'] == 0:
+                    log("Invalid ip address given from address: " + address + " with cip: " + cip)
+                    # Closing connection
+                    closeConnection(address, " Due to unknown command '" + strdata + "'")
+                    answer = "con-res 0xFE"
+        elif address in clients and clients[address]["state"] == "connected" and (time.time() - clients[address]["time"]) < 4 and clients[address]['locked'] == 0:
             clients[address]['locked'] = 1
             if time.time() - clients[address]["time"] >= 4:
                 # Makes sure that we are not terminating the connection while we are using it
@@ -167,19 +192,22 @@ while True:
                     answer = "con-res 0xFE"
                 else:
                     # Do nothing
-                    print("Do nothing")
+                    log("Do nothing")
                     pass
             if address in clients:
                 clients[address]['locked'] = 0
-
+        else:
+            # Unknown or invalid command resetting connection
+            closeConnection(address, " Due to unknown or invalid command '" + strdata + "'")
+            answer = "con-res 0xFE"
         if len(answer) > 0:
-    #        print("sendadress", sendaddress[0], sendaddress[1], type(sendaddress))
+    #        log("sendadress", sendaddress[0], sendaddress[1], type(sendaddress))
             sent = send(answer, sendaddress)
-            if debug: print('sent {} bytes back to {}'.format(sent, sendaddress), answer)
+            log('sent '+str(sent)+' bytes back to ' + str(sendaddress) + ' in the form of answer \'' + answer + '\'')
     except ConnectionResetError as cre:
-        print("Connection was badly closed by client")
+        log("Remote host closed connection")
     except Exception as e:
-        print("Server crashed! Server has restarted..")
+        log("Server crashed! Server has restarted.. " + str(e))
 
 #END###############################
 #END###### CODE ###################
